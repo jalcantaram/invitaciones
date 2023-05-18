60c24352ff7beed6ad32ef4d34f236b6393ef13e
JC <jcarlosalcantaral.mtz@gmail.com>
2023-05-18 01:41:19 -0600

    FIX

--- plugins/invitaciones/landing/updates/create_seccion_tres_table.php
+++ plugins/invitaciones/landing/updates/create_seccion_tres_table.php
@@ -11,18 +11,18 @@ class CreateSeccionTresTable extends Migration
         Schema::create('invitaciones_landing_seccion_tres', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->increments('id');
+            $table->string('subtitulo_coctel')->nullable();
+            $table->string('donde_coctel')->nullable();
+            $table->string('hora_coctel')->nullable();
+            $table->string('link_googlemaps_coctel')->nullable();
             $table->string('subtitulo_ceremonia')->nullable();
             $table->string('donde_ceremonia')->nullable();
-            $table->string('cuando_ceremonia')->nullable();
             $table->string('hora_ceremonia')->nullable();
             $table->string('link_googlemaps_ceremonia')->nullable();
-            $table->string('subtitulo_recepcion')->nullable();
-            $table->string('donde_recepcion')->nullable();
-            $table->string('cuando_recepcion')->nullable();
-            $table->string('hora_recepcion')->nullable();
-            $table->string('link_googlemaps_recepcion')->nullable();
-            $table->string('text_color')->nullable();
-            $table->string('background_color')->nullable();
+            $table->string('subtitulo_cena')->nullable();
+            $table->string('donde_cena')->nullable();
+            $table->string('hora_cena')->nullable();
+            $table->string('link_googlemaps_cena')->nullable();
             $table->string('fecha_celebracion')->nullable();
             $table->timestamps();
         });
